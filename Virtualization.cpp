0ccbc8eb2dee5fc2a0a7f3c028d6935e0b7ffa20
1401199262 <55327538+1401199262@users.noreply.github.com>
2022-03-09 20:45:01 +0800

    Update Virtualization.cpp

--- Virtualization.cpp
+++ Virtualization.cpp
@@ -140,15 +140,6 @@ int main()
 		//m.unlock();
 		//R/W ==========================================================================================
 
-		//Ptr
-		//m.lock();
-		//*(DWORD64*)((DWORD64)hidemem + 8) = (DWORD64)&MessageBox;
-		//printf("Bytes In MessageBoxA %llx\n", **(DWORD64**)((DWORD64)hidemem + 8));
-		//if(MessageBoxState)
-		//	MessageBoxState = ((decltype(&MessageBox))(*(DWORD64*)((DWORD64)hidemem + 8)))(0, L"Continue Executing MessageBox?", L"LOL", MB_YESNO) == IDNO ? FALSE : TRUE;
-		//m.unlock();
-
-
 		//Execute =======================================================================================
 		//tick = GetTickCount64(); 请查看当前文件头部的锁
 		//m.lock();
@@ -168,23 +159,7 @@ int main()
 		//m.unlock();
 		//SEH ===========================================================================================
 
-
-		//For Safety ====================================================================================
-		//m.lock();
-		if (!IsMemoryHidden())
-		{
-			ReadThread1.detach();
-			ReadThread2.detach();
-
-			//m.unlock();
-			std::cout << "Error: DONT debug the critical R/W/X Code" << std::endl;
-			system("pause");
-			exit(0);
-		}
-		//m.unlock();
-		//For Safety ====================================================================================
-
-		Sleep(200);
+		//Sleep(200);
 		system("cls");
 	}
 
@@ -194,46 +169,3 @@ END:
 	//m.unlock();
 }
 
-
-
-//int main()
-//{
-//    ZyanU8 data[] =
-//    {
-//        0x51, 0x8D, 0x45, 0xFF, 0x50, 0xFF, 0x75, 0x0C, 0xFF, 0x75,
-//        0x08, 0xFF, 0x15, 0xA0, 0xA5, 0x48, 0x76, 0x85, 0xC0, 0x0F,
-//        0x88, 0xFC, 0xDA, 0x02, 0x00
-//    };
-//
-//    // Initialize decoder context
-//    
-//    ZydisDecoderInit(&decoder, ZYDIS_MACHINE_MODE_LONG_64, ZYDIS_ADDRESS_WIDTH_64);
-//
-//    // Initialize formatter. Only required when you actually plan to do instruction
-//    // formatting ("disassembling"), like we do here
-//    ZydisFormatter formatter;
-//    ZydisFormatterInit(&formatter, ZYDIS_FORMATTER_STYLE_INTEL);
-//
-//    // Loop over the instructions in our buffer.
-//    // The runtime-address (instruction pointer) is chosen arbitrary here in order to better
-//    // visualize relative addressing
-//    ZyanU64 runtime_address = 0x007FFFFFFF400000;
-//    ZyanUSize offset = 0;
-//    const ZyanUSize length = sizeof(data);
-//    ZydisDecodedInstruction instruction;
-//    while (ZYAN_SUCCESS(ZydisDecoderDecodeBuffer(&decoder, data + offset, length - offset,
-//        &instruction)))
-//    {
-//        // Print current instruction pointer.
-//        printf("%016" PRIX64 "  ", runtime_address);
-//
-//        // Format & print the binary instruction structure to human readable format
-//        char buffer[256];
-//        ZydisFormatterFormatInstruction(&formatter, &instruction, buffer, sizeof(buffer),
-//            runtime_address);
-//        puts(buffer);
-//
-//        offset += instruction.length;
-//        runtime_address += instruction.length;
-//    }
-//}
